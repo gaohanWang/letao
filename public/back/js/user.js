18c08bf7e3133948399d5e0b7262be370a3830bc
wanggaohan <805429918@qq.com>
2017-10-31 17:26:27 +0800

    user完成,用户管理完成

--- public/back/js/user.js
+++ public/back/js/user.js
@@ -36,6 +36,38 @@ $(function () {
      }
   });
   }
-  
   render();
+  
+  //点击btn 弹出模态框
+  //给tbody绑定委托事件
+  $("tbody").on("click",".btn",function () {
+    //是模态框显示
+    $("#userModal").modal("show");
+    //改变数据 拿到id值  判断isDelete
+    var id= $(this).parent().data("id");
+    var isDelete = $(this).parent().data("isDelete");
+    console.log(isDelete);
+    isDelete = isDelete === 1 ? 0 : 1;
+    //点击btn按钮 可以进行切换  解除绑定事件
+    $('.btn_confirm').off().on("click",function () {
+      $.ajax({
+        type:"post",
+        url:"/user/updateUser",
+        data:{
+          id:id,
+          isDelete:isDelete
+        },
+        success:function (data) {
+          console.log(data);
+          //拿到数据关掉模态框
+          if(data.success){
+            //关闭模态框
+            $("#userModal").modal("hide");
+            render();
+          }
+        }
+      });
+    });
+    
+  });
 })
